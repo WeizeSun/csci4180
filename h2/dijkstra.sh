1cc3efaa3d0c94b700eb601b4758bc5b4a6eb651
sophiroth <sunweize36@126.com>
2017-11-20 03:11:17 +0000

    I don't want to see the modified

--- h2/dijkstra.sh
+++ h2/dijkstra.sh
@@ -5,12 +5,10 @@ hadoop fs -rm -R /p1output
 hadoop fs -rm -R /temp*
 rm -R preprocessOutput
 rm -R p1output
-# hadoop com.sun.tools.javac.Main h2p1/PDPreProcess.java
-# hadoop com.sun.tools.javac.Main h2p1/PDNodeWritable.java
-# hadoop com.sun.tools.javac.Main h2p1/ParallelDijkstra.java
+
 javac -cp $(hadoop classpath):. h2p1/*.java
 jar cf pd.jar h2p1/*.class
-# hadoop jar pd.jar h2p1.PDPreProcess /input /preprocessOutput
-# hadoop fs -get /preprocessOutput
-hadoop jar pd.jar h2p1.ParallelDijkstra /input /p1output $1 $2
+hadoop jar pd.jar h2p1.PDPreProcess /input /preprocessOutput
+hadoop fs -get /preprocessOutput
+hadoop jar pd.jar h2p1.ParallelDijkstra /input/test_dist.txt /p1output $1 $2
 hadoop fs -get /p1output
--- h2/pagerank.sh
+++ h2/pagerank.sh
@@ -5,12 +5,10 @@ hadoop fs -rm -R /p2output
 hadoop fs -rm -R /temp*
 rm -R preprocessOutput
 rm -R p2output
-# hadoop com.sun.tools.javac.Main h2p1/PDPreProcess.java
-# hadoop com.sun.tools.javac.Main h2p1/PDNodeWritable.java
-# hadoop com.sun.tools.javac.Main h2p1/ParallelDijkstra.java
+
 javac -cp $(hadoop classpath):. h2p2/*.java
 jar cf pr.jar h2p2/*.class
 # hadoop jar pr.jar h2p1.PDPreProcess /input /preprocessOutput
 # hadoop fs -get /preprocessOutput
-hadoop jar pr.jar h2p2.PageRank /input /p2output $1 $2 $3
+hadoop jar pr.jar h2p2.PageRank /input/test_dist_tut.txt /p2output $1 $2 $3
 hadoop fs -get /p2output
